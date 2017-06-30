a55f68ae1c69bfe762cf70b2693a465e7bba9b55
NFS_MONSTR <nfsmonstr@gmail.com>
2017-06-30 12:30:14 +0300

    Added InitAll with custom name

--- VesaSDL.pas
+++ VesaSDL.pas
@@ -59,6 +59,7 @@ Interface
   Procedure InitSDL;{Init SDL2 only}
   Procedure InitWindow(full:boolean;name_:ansistring);{Create only window,to set size change apw and aph}
   Procedure InitAll(mode:word);{Init sdl and window with selected mode}
+  Procedure InitAll(mode:word; Name_:ansistring);{Init sdl and window with selected mode}
   Procedure InitAllX(x,y:word;full:boolean;Name_:ansistring);{Same but with custom settings}
   Procedure ChangeWindowResolution(w,h:word);{Change window resolution; Better to ClearScreen or just redraw it}
   Procedure SetWindowFullscreen(FullscreenType:integer);{0 - windowed, 1 - fake fullscreen(window to borderless with desktop size
@@ -484,14 +485,20 @@ procedure InitWindow(full:boolean;name_:ansistring);
    if debug then Writeln('Everything Done. Ready to work.')
 end;
 
-Procedure InitAll(mode:word);
+Procedure InitAll(mode:word; Name_:ansistring);
  var full:boolean;
  begin
   InitMode(mode);
   if mode>=$500 then
    full:=true else full:=false;
-  InitWindow(full,'VesaSDL app');
+  InitWindow(full,Name_);
  end;
+
+Procedure InitAll(mode:word);
+ begin
+  InitAll(mode,'VesaSDL app');
+ end;
+
 Procedure InitAllX(x,y:word;full:boolean;name_:ansistring);
  begin
   apw:=x;
